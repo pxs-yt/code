951d21a984c6c47b4fb2a42c68daf2f05da247c6
pxs-yt <158753404+pxs-yt@users.noreply.github.com>
2024-05-04 16:50:43 +0200

    Added button for opening .txt files from device

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
+// CLEAR BUTTON JS CODE
 const clearButton = document.getElementById("clear-button");
 
 clearButton.addEventListener("click", function() {
   const textArea = document.getElementById("text-area");
   textArea.value = "";
+});
+
+
+// OPEN FILE BUTTON JS CODE
+const openFileButton = document.getElementById("open-file-button");
+const fileInput = document.getElementById("file-input");
+
+openFileButton.addEventListener("click", function() {
+  fileInput.click();
+});
+
+fileInput.addEventListener("change", function() {
+  const reader = new FileReader();
+
+  reader.onload = function(e) {
+    const textArea = document.getElementById("text-area");
+    textArea.value = e.target.result;
+  };
+
+  reader.readAsText(fileInput.files[0]);
 });
